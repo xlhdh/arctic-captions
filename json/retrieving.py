a9a2daf94beed9d0105905473a718e0c5371efe5
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2015-11-04 13:08:45 -0500

    rbwb

--- json/retrieving.py
+++ json/retrieving.py
@@ -5,10 +5,11 @@ path = 'real'
 
 sp = []
 for i in range(1,9):
-	with open(path+'/train.pkl'+str(i*10000), 'wb') as f:
-		sp.append(pkl.load(f))
+with open(path+'/train.pkl'+str(i*10000), 'rb') as f:
+	print path+'/train.pkl'+str(i*10000)
+	sp.append(pkl.load(f))
 
-with open(path+'/train.pkl82783', 'wb') as f:
+with open(path+'/train.pkl82783', 'rb') as f:
 	sp.append(pkl.load(f))
 
 sp = vstack(sp,format='csr')
