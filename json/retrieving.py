24c1205900e1c6e384aeea148f2cb71b5ba1f502
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2015-11-04 13:04:32 -0500

    retro

--- json/retrieving.py
+++ json/retrieving.py
@@ -6,15 +6,15 @@ path = 'real'
 sp = []
 for i in range(1,9):
 	with open(path+'/train.pkl'+str(i*10000), 'wb') as f:
-		    sp.append(pkl.load(f))
+		sp.append(pkl.load(f))
 
 with open(path+'/train.pkl82783', 'wb') as f:
-	    sp.append(pkl.load(f))
+	sp.append(pkl.load(f))
 
 sp = vstack(sp,format='csr')
 
 with open(path+'/coco_align.train.feat.nd', 'wb') as f:
-	    sp.data.dump(f)
-	    sp.indices.dump(f)
-	    sp.indptr.dump(f)
+	sp.data.dump(f)
+	sp.indices.dump(f)
+	sp.indptr.dump(f)
 
