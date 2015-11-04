e3c44fe66c6562a99f72cc2d673fdfec96710e26
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2015-11-04 16:33:59 -0500

    pref

--- json/retrieving.py
+++ json/retrieving.py
@@ -19,7 +19,7 @@ for i in range(1,9):
 		sp.append(csr_matrix((lo(f),lo(f),lo(f))))
 
 with open(path+'/train82783.nd', 'rb') as f:
-	sp.append(csr_matrix(lo(f),lo(f),lo(f)))
+	sp.append(csr_matrix(l(o(f),lo(f),lo(f))))
 
 sv = reduce(conc, sp)
 del sp
