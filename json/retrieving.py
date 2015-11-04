95f2d71e259f57060a4b5b664683e6fedf01c5a1
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2015-11-04 16:23:44 -0500

    retrive

--- json/retrieving.py
+++ json/retrieving.py
@@ -1,6 +1,14 @@
 import cPickle as pkl
 from scipy.sparse import vstack
 
+def conc(matrix1, matrix2):
+    new_data = numpy.concatenate((matrix1.data, matrix2.data))
+    new_indices = numpy.concatenate((matrix1.indices, matrix2.indices))
+    new_ind_ptr = matrix2.indptr + len(matrix1.data)
+    new_ind_ptr = new_ind_ptr[1:]
+    new_ind_ptr = numpy.concatenate((matrix1.indptr, new_ind_ptr))
+    return csr_matrix((new_data, new_indices, new_ind_ptr))
+
 path = 'real'
 
 sp = []
@@ -12,10 +20,9 @@ for i in range(1,9):
 with open(path+'/train.pkl82783', 'rb') as f:
 	sp.append(pkl.load(f))
 
-sp = vstack(sp,format='csr')
+sv = reduce(conc, sp)
+del sp
 
 with open(path+'/coco_align.train.feat.nd', 'wb') as f:
-	sp.data.dump(f)
-	sp.indices.dump(f)
-	sp.indptr.dump(f)
+	pkl.dump(sv,f,protocol=pkl.HIGHEST_PROTOCOL)
 
