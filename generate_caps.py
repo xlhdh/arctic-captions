9bcc95c9ef4e018cdd203d537a563020c776e2ff
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2015-12-02 16:49:45 -0500

    new en_cap

--- generate_caps.py
+++ generate_caps.py
@@ -88,6 +88,7 @@ def main(model, saveto, k=5, normalize=False, zero_pad=False, n_process=5, datas
 
     # index -> words
     def _seqs2words(caps):
+        print 's2w'
         capsw = []
         for cc in caps:
             ww = []
@@ -108,9 +109,11 @@ def main(model, saveto, k=5, normalize=False, zero_pad=False, n_process=5, datas
             else:
                 cc0 = cc
             queue.put((idx, cc0))
+        return
 
     # retrieve caption from process
     def _retrieve_jobs(n_samples):
+        print 'retr'
         caps = [None] * n_samples
         for idx in xrange(n_samples):
             resp = rqueue.get()
@@ -126,21 +129,25 @@ def main(model, saveto, k=5, normalize=False, zero_pad=False, n_process=5, datas
         if dd == 'dev':
             print 'Development Set...',
             _send_jobs(valid[1])
+            print 'Finished sending DEV'
             caps = _seqs2words(_retrieve_jobs(len(valid[1])))
+            print 'Finished Generationg DEV'
             with open(saveto+'.dev.txt', 'w') as f:
                 print >>f, '\n'.join(caps)
             print 'Done'
         if dd == 'test':
             print 'Test Set...',
             _send_jobs(test[1])
+            print 'Finished sending TEST'
             caps = _seqs2words(_retrieve_jobs(len(test[1])))
+            print 'Finished Generationg TEST'
             with open(saveto+'.test.txt', 'w') as f:
                 print >>f, '\n'.join(caps)
             print 'Done'
     # end processes
     for midx in xrange(n_process):
         queue.put(None)
-
+    return 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
