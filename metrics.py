9db60631a5088683ae5940208491426964f0ff8f
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2015-12-03 01:58:23 -0500

    debug metrics

--- metrics.py
+++ metrics.py
@@ -29,6 +29,7 @@ def load_textfiles(references, hypothesis):
     print raw_refs[0]
     refs = {idx: rr for idx, rr in enumerate(raw_refs)}
     # sanity check that we have the same number of references as hypothesis
+    print len(hypo), len(refs)
     if len(hypo) != len(refs):
         raise ValueError("There is a sentence number mismatch between the inputs")
     return refs, hypo
