75c58007eb31005fc2d4b4135928cbe21377f20f
Eric <khrylx@gmail.com>
2015-12-09 16:00:16 -0500

    lol

--- generate_caps_ada.py
+++ generate_caps_ada.py
@@ -243,7 +243,7 @@ def main(model, saveto, k=5, normalize=False, zero_pad=False, n_process=5, datas
 
             with open(out_name, 'w') as f:
                 for i in range(len(scores)):
-                    modelWeight += float(weights[i])/totalWeight * scores[i] 
+                    modelWeight += float(weights[i])/totalWeight / scores[i] 
 
                     if scores[i] > 1.2*avgScore and weights[i] <= 10:
                         weights[i] = weights[i]+1
